436c922fcdbd5426b9775df0eba8fc3c75f6af4a
fallsea <xuemian@foxmail.com>
2020-08-22 17:13:05 +0800

    init

---
+++ wueasy-datamodel/src/test/java/Generator.java
@@ -0,0 +1,34 @@
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+/**
+ * 自动生成工具
+ * @author: fallsea
+ * @version 1.0
+ */
+public class Generator {
+
+	public static InputStream getResourceAsStream(String path) {
+		return Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+	}
+
+	public static void main(String[] args) throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(getResourceAsStream("generatorConfig.xml"));
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		myBatisGenerator.generate(null);
+		for (String warning : warnings) {
+			System.out.println(warning);
+		}
+	}
+
+}
